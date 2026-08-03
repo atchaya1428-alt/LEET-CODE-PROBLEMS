15a832f33bd28e5aa51fa0892cf1adb869510121
atchaya1428-alt <atchaya1428@gmail.com>
2026-08-03 11:41:13 +0530

    Implement island perimeter calculation

---
+++ 463-Island Perimeter.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int perimeter = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+
+                if (grid[i][j] == 1) {
+                    perimeter += 4;
+
+                    // Check cell above
+                    if (i > 0 && grid[i - 1][j] == 1) {
+                        perimeter -= 2;
+                    }
+
+                    // Check cell on the left
+                    if (j > 0 && grid[i][j - 1] == 1) {
+                        perimeter -= 2;
+                    }
+                }
+            }
+        }
+
+        return perimeter;
+    }
+}
